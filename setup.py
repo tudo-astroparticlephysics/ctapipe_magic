6ff9dea12f397e92b7ff6e8d0242517896ddc236
Noah Biederbeck <noah.biederbeck@udo.edu>
2018-10-26 09:01:59 +0200

    Use double hyphens

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ctapipe_magic',
+    name="ctapipe_magic",
     packages=find_packages(),
-    version='0.0.1',
-    description='Module to analyze MAGIC data with ctapipe',
+    version="0.0.1",
+    description="Module to analyze MAGIC data with ctapipe",
     install_requires=[
-        'uproot',
-        'ctapipe',
-        'ctapipe-extra'
+        "uproot",
+        "ctapipe",
+        "ctapipe-extra",
     ],
-    tests_require=['pytest', 'ctapipe-extra>=0.2.11'],
-    author='Noah Biederbeck',
-    author_email='noah.biederbeck@tu-dortmund.de',
-    license='mit',
-    url='https://github.com/tudo-astroparticlephysics/ctapipe_magic',
+    tests_require=[
+        "pytest",
+        "ctapipe-extra>=0.2.11",
+    ],
+    author="Noah Biederbeck",
+    author_email="noah.biederbeck@tu-dortmund.de",
+    license="mit",
+    url="https://github.com/tudo-astroparticlephysics/ctapipe_magic",
     zip_safe=False,
 )
