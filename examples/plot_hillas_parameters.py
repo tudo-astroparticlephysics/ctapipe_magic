ccda1cd3e2d7e539b8fa02f34ba20754bffe0bf2
Noah Biederbeck <noah.biederbeck@udo.edu>
2018-10-30 09:47:14 +0100

    add second telescope to example

--- examples/plot_hillas_parameters.py
+++ examples/plot_hillas_parameters.py
@@ -12,15 +12,17 @@ if __name__ == "__main__":
     # Load the camera
     geom = CameraGeometry.from_name(camera_id="MAGICCam")
 
-    fig, ax = plt.subplots()
+    fig, (ax1, ax2) = plt.subplots(ncols=2)
 
-    disp = CameraDisplay(geom, ax=ax)
-    disp.add_colorbar()
+    disp1 = CameraDisplay(geom, ax=ax1)
+    disp1.add_colorbar()
+    disp2 = CameraDisplay(geom, ax=ax2)
+    disp2.add_colorbar()
 
     # Get the eventsource
     source = MAGICIOEventSource(
         # input_url="/data/20181011_M1_05075881.001_Y_CrabNebula-W0.40+359.root"
-        input_url="/data/magic_test.root"
+        input_url="/data/GA_M1_za05to35_9_823708_Y_w0.root"
     )
 
     # get a single image
@@ -28,27 +30,33 @@ if __name__ == "__main__":
         # Pick a (random) good looking image
         if i > 147:
             break
-    im = img.dl1.tel[0].image
+    im1 = img.dl1.tel[0].image
+    im2 = img.dl1.tel[1].image
     print(img)
 
     # Apply image cleaning
-    cleanmask = tailcuts_clean(
-        geom,
-        im,
-        picture_thresh=30,
-        boundary_thresh=5,
-        min_number_picture_neighbors=0,
+    cleanmask1 = tailcuts_clean(
+        geom, im1, picture_thresh=30, boundary_thresh=5, min_number_picture_neighbors=0
+    )
+    cleanmask2 = tailcuts_clean(
+        geom, im2, picture_thresh=30, boundary_thresh=5, min_number_picture_neighbors=0
     )
 
-    if sum(cleanmask) == 0:
+    if sum(cleanmask1) == 0:
         pass
     else:
         # Calculate image parameters
-        hillas = hillas_parameters(geom[cleanmask], im[cleanmask])
+        hillas1 = hillas_parameters(geom[cleanmask1], im1[cleanmask1])
+        hillas2 = hillas_parameters(geom[cleanmask2], im2[cleanmask2])
 
         # Show the camera image and overlay Hillas ellipse and clean pixels
-        disp.image = im
-        disp.highlight_pixels(cleanmask, color="crimson")
-        disp.overlay_moments(hillas, color="red", linewidth=2)
+        disp1.image = im1
+        disp1.highlight_pixels(cleanmask1, color="crimson")
+        disp1.overlay_moments(hillas1, color="red", linewidth=2)
 
-        plt.savefig("build/hillas_example.png", dpi=200)
+        disp2.image = im2
+        disp2.highlight_pixels(cleanmask2, color="crimson")
+        disp2.overlay_moments(hillas2, color="red", linewidth=2)
+
+        fig.tight_layout()
+        fig.savefig("build/hillas_example.png", dpi=200)
